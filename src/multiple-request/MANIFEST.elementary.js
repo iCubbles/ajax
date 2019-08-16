95c537bb17a6c05c063e7e28f6b433faa1d17e31
Philipp Wagner <wagner@getrequest.de>
2019-08-16 13:44:24 +0200

    add and migrate ajax-multpiple-request component (#5)

--- src/multiple-request/MANIFEST.elementary.js
+++ src/multiple-request/MANIFEST.elementary.js
@@ -3,23 +3,24 @@ const assert = require('assert');
 module.exports = (webpackageName) => {
   assert.ok(webpackageName, 'Expected "webpackageName" to be defined.');
   return {
-    description: 'Component for making a single ajax request. Uses axios lib under the hood.',
+    description: 'Component for making a bunch of ajax request in parallel. Uses axios lib under the hood.',
     slots: [
       {
-        slotId: 'config',
+        slotId: 'requests',
         direction: [ 'input' ],
         type: 'object',
-        description: 'this needs to be a valid axios config object, see https://github.com/mzabriskie/axios#request-config'
+        description: 'Accepts an array of axios config objects each representing a request to be made. Each item needs to be a valid axios config object, see https://github.com/mzabriskie/axios#request-config'
       },
       {
         slotId: 'status',
         direction: [ 'output' ],
         type: 'string',
-        description: 'will be one of idle|busy|error'
+        description: 'Indicates the internal status of the component instance. One of [\'idle\',\'pending\',\'error\']'
       },
       {
-        slotId: 'result',
+        slotId: 'results',
         direction: [ 'output' ],
+        description: 'Holds an array of response objects. The index of a response objects equals the index of the corresponding request in requests array.',
         type: 'object'
       }
     ],
